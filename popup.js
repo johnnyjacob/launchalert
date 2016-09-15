1d14132a443ac4f5dc99bc77c50e58312108cd4d
Johnny Jacob <johnnyjacob@gmail.com>
2016-09-15 11:00:12 +0530

    Fix Typo. Fetch next launch and display the JSON.

--- popup.js
+++ popup.js
@@ -1,10 +1,20 @@
 (function(){
+    var nextFiveLaunches = 'https://launchlibrary.net/1.2/launch/next/1';
+
     function renderStatus(statusText) {
 	document.getElementById('status').textContent = statusText;
     }
 
+    function fetchFailed (errorMessage) {
+    }
+
+    function updateLaunchTable(launchData) {
+	renderStatus (launchData);
+    }
+
     function main() {
-	renderStatus('Nothing here yet! ');
+	renderStatus('Fetching...');
+	launchalert.requestURL(nextFiveLaunches, "text", updateLaunchTable);
     }
     main();
 })();

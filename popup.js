15d263d5b7a3be87490c97a896ba34d3254468db
Johnny Jacob <johnnyjacob@gmail.com>
2016-09-19 17:03:11 +0530

    Parse the result and display the results separately.

--- popup.js
+++ popup.js
@@ -1,19 +1,30 @@
+// Copyright (c) 2016, Johnny Jacob <johnnyjacob@gmail.com>  All rights reserved.
+// Copyrights licensed under the BSD 3 Clause License.
+// See the accompanying LICENSE file for terms.
+
 (function(){
     var nextFiveLaunches = 'https://launchlibrary.net/1.2/launch/next/1';
 
-    function renderStatus(statusText) {
-	document.getElementById('status').textContent = statusText;
+    function renderLaunchTable (launchData) {
+	var launchList = JSON.parse (launchData);
+
+	document.getElementById('missionname').textContent = launchList.launches[0].name;
+	document.getElementById('launchschedule').textContent = launchList.launches[0].net;
+	document.getElementById('launchstatus').textContent = launchList.launches[0].status;
+
+	chrome.browserAction.setBadgeText({text:"1d"});
+	chrome.browserAction.setBadgeBackgroundColor({color:[255, 0, 0, 255]});
     }
 
     function fetchFailed (errorMessage) {
+	console.log (errorMessage);
     }
 
     function updateLaunchTable(launchData) {
-	renderStatus (launchData);
+	renderLaunchTable(launchData);
     }
 
     function main() {
-	renderStatus('Fetching...');
 	launchalert.requestURL(nextFiveLaunches, "text", updateLaunchTable);
     }
     main();
